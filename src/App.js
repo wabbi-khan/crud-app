abef024b0ac4c9e00f0b3683300e6f06cd694dfc
Wahab Khan <kwahab789@gmail.com>
2021-11-28 19:14:56 +0500

    routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
-
+import Navbar from "./components/layout/Navbar";
+import About from "./components/pages/About";
+import Contact from "./components/pages/Contact";
+import Home from "./components/pages/Home";
 function App() {
   return (
     <>
-      <div>hello</div>
+      <Navbar />
+      <Home />
+      <About />
+      <Contact />
     </>
   );
 }
